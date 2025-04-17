05284044c4a69072f24c74eb14518d8e590074e2
NuwanSD <sagaradnuwan@gmail.com>
2025-04-17 17:32:34 +0530

    added auth logic with the fetching functions

--- client/src/features/user/pages/Auth.jsx
+++ client/src/features/user/pages/Auth.jsx
@@ -1,18 +1,20 @@
 import Card from "../../../app/shared/components/UIElements/Card";
 import Button from "../../../app/shared/components/FormElements/Button";
 import Input from "../../../app/shared/components/FormElements/Input";
+import ErrorModal from "../../../app/shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../../app/shared/components/UIElements/LoadingSpinner";
+
 import { useForm } from "../../../app/shared/hooks/form-hook";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../../app/shared/context/auth-context";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from "../../../app/shared/util/validators";
-import { useContext, useState } from "react";
-import { AuthContext } from "../../../app/shared/context/auth-context";
-
 import "./Auth.css";
-import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Auth() {
   const auth = useContext(AuthContext);
@@ -22,6 +24,8 @@ export default function Auth() {
   const from = location.state?.from?.pathname || "/";
 
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const [formState, inputHandler, setFormData] = useForm({
     email: {
@@ -58,54 +62,120 @@ export default function Auth() {
     setIsLogin((prevMode) => !prevMode);
   };
 
-  const authSubmitHandler = (event) => {
+  const authSubmitHandler = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
-    auth.login();
-    navigate(from, { replace: true });
+
+    if (isLogin) {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+        auth.login();
+        navigate(from, { replace: true });
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setError(error.message || "Something went wrong, please try again");
+      }
+    } else {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch("http://localhost:5000/api/users/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+        auth.login();
+        navigate(from, { replace: true });
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setError(error.message || "Something went wrong, please try again");
+      }
+    }
+  };
+
+  const errorHandler = () => {
+    setError(null);
   };
 
   return (
-    <Card className="authentication">
-      <h2>Login Required</h2>
-      <hr />
-      <form onSubmit={authSubmitHandler}>
-        {!isLogin && (
+    <div>
+      <ErrorModal error={error} onClear={errorHandler} />
+      <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay />}
+        <h2>Login Required</h2>
+        <hr />
+        <form onSubmit={authSubmitHandler}>
+          {!isLogin && (
+            <Input
+              element="input"
+              id="name"
+              type="text"
+              label="Your Name"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="Please enter a name"
+              onInput={inputHandler}
+            />
+          )}
           <Input
             element="input"
-            id="name"
-            type="text"
-            label="Your Name"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a name"
+            id="email"
+            type="email"
+            label="E-mail"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid email"
             onInput={inputHandler}
           />
-        )}
-        <Input
-          element="input"
-          id="email"
-          type="email"
-          label="E-mail"
-          validators={[VALIDATOR_EMAIL()]}
-          errorText="Please enter a valid email"
-          onInput={inputHandler}
-        />
-        <Input
-          element="input"
-          id="password"
-          type="password"
-          label="Password"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid password"
-          onInput={inputHandler}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLogin ? "LOGIN" : "SIGNUP"}
+          <Input
+            element="input"
+            id="password"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid password"
+            onInput={inputHandler}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLogin ? "LOGIN" : "SIGNUP"}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          SWITCH TO SIGNUP
         </Button>
-      </form>
-      <Button inverse onClick={switchModeHandler}>
-        SWITCH TO SIGNUP
-      </Button>
-    </Card>
+      </Card>
+    </div>
   );
 }
